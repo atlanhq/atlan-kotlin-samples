46ee03eb7f0731049400f4a3e95149d75b252221
Christopher Grote <cmgrote@users.noreply.github.com>
2023-10-04 13:57:04 +0100

    Bundle Numaflow as part of events jar
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- events/build.gradle.kts
+++ events/build.gradle.kts
@@ -3,17 +3,29 @@ val jarFile = "events-$version.jar"
 
 plugins {
     id("atlan-kotlin-sample")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 dependencies {
     implementation(project(":common"))
     implementation(project(":serde"))
-    implementation("io.numaproj.numaflow:numaflow-java:0.4.6")
+    api("io.numaproj.numaflow:numaflow-java:0.4.6")
+    api("io.netty:netty-transport-native-epoll:4.1.94.Final:linux-x86_64")
 }
 
 tasks {
-    jar {
+    shadowJar {
+        isZip64 = true
         archiveFileName.set(jarFile)
         destinationDirectory.set(file(jarPath))
+        dependencies {
+            include(dependency("io.numaproj.numaflow:numaflow-java:.*"))
+            include(dependency("io.netty:netty-transport-native-epoll:4.1.94.Final:linux-x86_64"))
+        }
+        mergeServiceFiles()
+    }
+
+    jar {
+        dependsOn(shadowJar)
     }
 }
