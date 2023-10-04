d89980fd2cde58fc8d4a0a6271621cf977c28b60
Christopher Grote <cmgrote@users.noreply.github.com>
2023-10-04 14:39:27 +0100

    Latest plain Java SDK base image
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- events/build.gradle.kts
+++ events/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     implementation(project(":common"))
     implementation(project(":serde"))
     api("io.numaproj.numaflow:numaflow-java:0.4.6")
-    api("io.netty:netty-transport-native-epoll:4.1.94.Final:linux-x86_64")
+    api("io.netty:netty-transport-native-epoll:4.1.79.Final:linux-x86_64")
 }
 
 tasks {
@@ -20,7 +20,7 @@ tasks {
         destinationDirectory.set(file(jarPath))
         dependencies {
             include(dependency("io.numaproj.numaflow:numaflow-java:.*"))
-            include(dependency("io.netty:netty-transport-native-epoll:4.1.94.Final:linux-x86_64"))
+            include(dependency("io.netty:netty-transport-native-epoll:4.1.79.Final:linux-x86_64"))
         }
         mergeServiceFiles()
     }
